2ba93a950f0ef63dade2d0d4b36b284513fbb0c6
David Sancho <dsnxmoreno@gmail.com>
2015-10-20 22:44:54 +0200

    Add prefixes to config

--- config.js
+++ config.js
@@ -26,6 +26,17 @@ export default {
       sourcemap: true,
       verbose: true
     },
+    prefixOptions: {
+      map: { inline: true },
+      browsers: [
+        'last 2 version',
+        'safari 5',
+        'ie 9',
+        'opera 12.1',
+        'ios 6',
+        'android 4'
+      ]
+    }
   },
   server: {
     baseDir: './public'
