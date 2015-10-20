f43f8c606d47b8e297b375b9821bae1f4cbbe4a1
David Sancho <dsnxmoreno@gmail.com>
2015-10-21 00:05:15 +0200

    Adding messages to the config

--- config.js
+++ config.js
@@ -3,24 +3,28 @@ export default {
   html: {
     input: 'src/',
     output:'public/',
-    all:   'src/**/*.html'
+    all:   'src/**/*.html',
+    message: 'htmlifying...'
   },
   assets: {
     input: 'src/',
     output:'public/assets',
-    all:   'src/assets/**/*'
+    all:   'src/assets/**/*',
+    message: 'assets...'
   },
   scripts: {
     input: 'src/scripts/app.js',
     output:'public/scripts',
     all:   'src/scripts/**/*.js',
-    public:'build.js'
+    public:'build.js',
+    message: 'watchifying...'
   },
   styles: {
     input: 'src/',
     output:'public/styles',
     all:   'src/styles/**/*.scss',
     public:'style.css',
+    message: 'sassifying...',
     sassOptions: {
       outputStyle: 'compressed',
       sourcemap: true,
@@ -56,6 +60,7 @@ export default {
       clean: true,
       exclude: ['.DS_Store'],
       include: []
-    }
+    },
+    message: 'deploying...'
   }
 };
